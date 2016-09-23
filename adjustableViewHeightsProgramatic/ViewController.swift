95c65cb727e641e6b01b3570b74c1519a680713a
Adhyam Nagarajan, Padmaja <paadhyamnagaraja@paypal.com>
2016-09-23 11:13:38 -0700

    Initial Commit

---
+++ adjustableViewHeightsProgramatic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  adjustableViewHeightsProgramatic
+//
+//  Created by Adhyam Nagarajan, Padmaja on 9/23/16.
+//  Copyright © 2016 Adhyam Nagarajan, Padmaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
