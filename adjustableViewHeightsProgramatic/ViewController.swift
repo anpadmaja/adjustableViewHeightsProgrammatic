3ab4c1d9610edc000546f3004e7554663fe7328a
Adhyam Nagarajan, Padmaja <paadhyamnagaraja@paypal.com>
2016-09-24 11:46:01 -0700

    initial commit

--- adjustableViewHeightsProgramatic/ViewController.swift
+++ adjustableViewHeightsProgramatic/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  typealias onReload = Void -> Void
+  var reload:onReload?
+  @IBOutlet weak var clickME: UIButton!{
+    didSet{
+      self.clickME.setTitle("Click to see your Poker face", forState: UIControlState.Normal)
+    }
+  }
+  
+  @IBOutlet weak var bradMeImage: UIImageView!
+  
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var containerViewHeight: NSLayoutConstraint!
+  
+  @IBAction func toggleAction(sender: AnyObject) {
+    self.bradMeImage.hidden = false
+    self.clickME.setTitle("Here you go!!!!!", forState: UIControlState.Normal)
+//    self.bradMeImage.layoutIfNeeded()
+//    self.bradMeImage.setNeedsDisplay()
+    self.bradMeImage.layoutIfNeeded()
+    self.containerViewHeight.constant = self.containerViewHeight.constant + 10
+    self.containerView.layoutIfNeeded()
+//    self.reload?()
+//    self.bradMeImage.setNeedsDisplay()
+//        self.containerView.setNeedsDisplay()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.bradMeImage.hidden = true
+    if(self.bradMeImage.hidden){
+      self.containerViewHeight.constant = self.containerViewHeight.constant - self.bradMeImage.frame.height
+    }
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
   }
 
   override func didReceiveMemoryWarning() {
